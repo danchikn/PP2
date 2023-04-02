38873c123688155f1c98fdb603cd632fd9cdc7b5
Dana Sadanova <123665971+danchikn@users.noreply.github.com>
2023-04-03 01:15:01 +0600

    Delete numbers directory

--- numbers/2.py
+++
@@ -1,7 +0,0 @@
-x = 102      # int
-y = 2.438    # float
-z = 1j       # complex(why j?)
-
-print(type(x))
-print(type(y))
-print(type(z))
--- numbers/4.py
+++
@@ -1,7 +0,0 @@
-x = 1.10465455
-y = 1.0
-z = -35.593545
-
-print(type(x))
-print(type(y))
-print(type(z))
